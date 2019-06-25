d32ad57ac1010c9b8b0e55211857d6ef16c21774
Rob <rob@MacBook1.local>
2019-06-25 09:02:07 -0700

    Initial Commit

---
+++ src/components/AlbumList.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import AlbumDetail from "./AlbumDetail";
+
+
+
+class AlbumList extends Component {
+        state = { albums: [] };
+
+        componentWillMount() {
+            fetch('https://rallycoding.herokuapp.com/api/music_albums')
+                .then(response => response.json())
+                .then(rdata => { this.setState({albums: rdata})}
+            );
+        }
+
+        renderAlbums() {
+            return this.state.albums.map(album => 
+            <AlbumDetail key={album.title} album = {album} />
+            );
+        }
+
+        render() {
+            console.log(this.state);
+        return (
+            <View>
+                { this.renderAlbums()}
+            </View>
+        );
+
+    }
+}
+
+export default AlbumList;
